892be03bc20b9cf8927efc2495ffaf5f8816ab87
Alexander G. Yang <spark8402@rogers.com>
2019-07-02 23:01:33 -0400

    assignment done

--- team.test.js
+++ team.test.js
@@ -1,5 +1,10 @@
 const teamObj = require('./team.js');
 
-// test('', () => {
-//
-// });
+test('checks the structure of the object it returns', () => {
+    const result = teamObj('Judoboy', 100);
+
+    expect(result.teamName).toBe('Judoboy');
+    expect(result.level).toBe(100);
+    expect(result.points).toBe(0);
+  });
+
